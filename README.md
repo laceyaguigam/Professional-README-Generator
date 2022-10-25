3f55b299aed6bff84eea0e744783500482725794
lacey aguigam <laceyaguigam@yahoo.com>
2022-10-25 15:50:55 -0700

    created final README.md using node

--- README.md
+++ README.md
@@ -1,14 +1,43 @@
-# The title of my project is challenge 9
+# README Generator
  
 ## Description
 
-The purpose of this project is to create a professional README using node
+ This program will allow you to create a professional README by simply answering the generated questions. The program will then place the answers in the corresponding section of the README file.
 
-## License
+##Table of Contents
 
-The project was built with [![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0) 
+- Usage
+- Installation
+- Test
+- Contributions
+- Questions
+
+## Usage
+
+Copy the code, once code is saved on your local machine, you will be able to run node and answer the questions in order to create a README.md.
+
+## Installation
+
+undefined
+
+The project was built with undefined 
 
 
-The URL for the project is https://github.com/laceyaguigam/Professional-README-Generator
+## Test 
 
-I was the only one who contributed to this project
+The URL for the project is https://github.com/laceyaguigam/Professional-README-Generator  laceyaguigam
+
+## Screenshot & Demo
+
+
+## Contribution Guidelines
+
+Contact me if further instructions are needed.  contributed to this project
+
+## Questions 
+
+For questions you may reach me at https://github.com/laceyaguigam and at laceyaguigam@yahoo.com.
+
+
+
+Copy or clone  your generated code onto your local machine.
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // TODO: Include packages needed for this application
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 
 
 // TODO: Create an array of questions for user input
+
 const questions = [
     {
         message: "What is the title of your project?",
@@ -54,6 +56,7 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
         if(err) throw err;
@@ -62,6 +65,7 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
+
 function init() {
     inquirer.prompt(questions)
     .then(answers => {
@@ -101,52 +105,29 @@ ${answers.installation}
 The project was built with ${answers.licenseBadge} 
 
 
-##Test 
+## Test 
 
 The URL for the project is ${answers.url}
+
+## Screenshot & Demo
 
 
 ## Contribution Guidelines
 
-${answers.contributed} contributed to this project`
+${answers.contributed} contributed to this project
 
 ## Questions 
 
-For questions you may reach me at ${url} and at ${email}.
+For questions you may reach me at ${answers.url} and at ${answers.email}.
 
 
 
-${instructions}
+${answers.instructions}`
 
-        console.log(template)
-        writeToFile("README.md", template)
+    //     console.log(template)
+       writeToFile("README.md", template)
     })
 }
 
 // Function call to initialize app
 init();
-
-
-
-
-
-
-
-//The generated Readme is missing a table of contents, a badge, and an email. 
-//It also does not include all the required sections.
-
-//The Readme does not include a link to the walkthrough video. 
-//Commit often using descriptive messages of your code. 
-//Make sure that node_modules is not included in your repo. 
-
-
-//WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-//THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-
-
-
-
-
-
-
